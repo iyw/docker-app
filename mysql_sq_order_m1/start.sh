e685a57768fb6b077b274234e3504394ec697ae2
tanyawen <tanyawen@01zhuanche.com>
2020-04-21 10:46:53 +0800

    fix

--- mysql_sq_order_m1/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker stop mysql_sq_order_m1
-docker rm mysql_sq_order_m1
-docker run -itd --name mysql_sq_order_m1 -p 4407:3306  -v `pwd`/conf.d:/etc/mysql/conf.d -v `pwd`/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root hub.c.163.com/library/mysql:5.7
--- mysql_sq_order_total/replication-cnf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-  echo "[mysqld]" >> sq_order.cnf;
-  for((i=0; $i<512; i++)); do
-    echo "replicate-rewrite-db=sq_order_${i}->sq_order" >> sq_order.cnf;
-  done
